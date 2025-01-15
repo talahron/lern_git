03c3fa98d10dbd3490e319eb581c31c24190e079
Tal Aharon <64023164+talahron@users.noreply.github.com>
2025-01-15 16:20:00 +0200

    Update turtle_game.py

--- turtle_game.py
+++ turtle_game.py
@@ -2,21 +2,17 @@ import turtle
 import colorsys
 import random
 
-# הגדרת המסך
 screen = turtle.Screen()
 screen.bgcolor("black")
 screen.title("ספירלה צבעונית עם Turtle")
 
-# יצירת הצב
 spiral = turtle.Turtle()
 spiral.speed(0)  # מהירות מקסימלית
 spiral.width(2)
 
-# מספר הצבעים בספירלה
 num_colors = 360
 hue = 0
 
-# פונקציה לצביעת הספירלה
 def draw_spiral():
     global hue
     for i in range(360):
@@ -29,9 +25,10 @@ def draw_spiral():
         if hue > 1:
             hue = 0
 
-# קריאת הפונקציה לציור הספירלה
+
 draw_spiral()
 
-# סיום התצוגה
+
 spiral.hideturtle()
 turtle.done()
+
